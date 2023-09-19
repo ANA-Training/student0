5749c709effc98c98d946b95089da3024ea3c600
abdtyx <1507844577@qq.com>
2023-09-19 23:29:21 +0800

    Publish upgrade-service

---
+++ upgrade-service/main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Feel free to add any global variable, define, typedef, or anything you like
+
+int main() {
+	// YOUR CODE HERE
+
+	return 0;
+}
